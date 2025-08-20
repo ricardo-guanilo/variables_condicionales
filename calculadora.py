789c5f4bd7ee0840af7d96240cfab12644a3e398
ricardo-guanilo <ricardo.guanilo.p@uni.pe>
2025-08-20 17:52:41 -0500

    hola

---
+++ calculadora.py
@@ -0,0 +1,46 @@
+operacion = input(f'''{'':*^90}
+*{'Ingrese la operación a realizar:':^88}*
+{'':*^90}
+*{'1. Suma':^88}*
+*{'2. Resta':^88}*
+*{'3. Producto':^88}*
+*{'4. División':^88}*
+{'':*^90}
+''')
+print(f'{'':*^90}')
+if operacion not in ['1','2','3','4']:
+    print(f'ERROR: ingrese un número valido (1,2,3,4), valor ingresado actualmente: {operacion}')
+else:
+    n1 = input('Ingrese el primer número: ')
+    n2 = input('Ingrese el segundo número: ')
+    print(f'{'':*^90}')
+
+    # Verificamos si n1 y n2 son enteros
+    try: 
+        temp = int(n1) + int(n2)
+    except ValueError:
+        print(f'''ERROR: Ingrese un número real, valores ingresados actualmente:
+        {n1}, {n2}''')
+    # Tranformamos a número las cadenas n1 y n2
+    else:
+
+        a=float(n1) 
+        b=float(n2)
+        match operacion:
+            case '1':
+                operacion = 'Suma'
+                print(f'Se realizo la operación {operacion.lower()}: {a} + {b} = {a+b:.2f}')
+            case '2':
+                operacion = 'Resta'
+                print(f'Se realizo la operación {operacion.lower()}: {a} - {b} = {a-b:.2f}')
+            case '3':
+                operacion = 'Producto'
+                print(f'Se realizo la operación {operacion.lower()}: {a} * {b} = {a*b:.2f}')
+            case '4':
+                operacion = 'División'
+                if b == 0:
+                    print('ERROR: Ingrese un denominador distinto de cero')
+                else:
+                    print(f'Se realizo la operación {operacion.lower()}: {a} / {b} = {a/b:.2f}')
+
+
