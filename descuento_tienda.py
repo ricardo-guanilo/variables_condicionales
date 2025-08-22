46da71f08723e76d19df93cd602e4d8a2f8567b2
ricardo-guanilo <ricardo.guanilo.p@uni.pe>
2025-08-22 13:26:22 -0500

    Upload del archivo descuento_tienda.py

--- descuento_tienda.py
+++ descuento_tienda.py
@@ -1,16 +1,16 @@
 try:
     precio = input('Ingrese el precio final del producto: ')
-    precio = float(precio)
-    tarjeta = input('¿Tiene tarjeta de fidelidad? (Y/N): ').upper()
+    precio = float(precio) # Verificación de tipo de dato de precio, salta error de ser necesario
+    tarjeta = input('¿Tiene tarjeta de fidelidad? (Y/N): ').upper() # Permite ingreso de mayúsculas o minúsculas
     print(f'{'':*^90}')
 except:
     print(f"ERROR: ingrese un numero real en su precio, precio ingresado: {precio}")
 else:
-    if (tarjeta != 'Y') and (tarjeta !='N'):
+    if (tarjeta != 'Y') and (tarjeta !='N'): # Verificar si se ingresó un valor distinto a Y o N
         print(f'ERROR: Por favor ingrese \"Y\" o \"N\", valor ingresado: {tarjeta}')
-    else:
+    else: # Aplicación de descuento acorde al rango del precio final del producto
         print(f'Precio incial: ${precio:.2f}')
-        if precio > 100000:
+        if precio > 100000: 
             descuento = 0.15
             precio_fin = precio * (1-descuento)
             print(f'Precio con descuento del {descuento*100}%: {precio_fin:.2f}.')
@@ -25,7 +25,7 @@ else:
         else:
             print('No se aplicó descuento por precio.')
             precio_fin = precio
-        match tarjeta:
+        match tarjeta: # Verificación si el cliente posee o no tarjeta de fidelidad.
             case 'Y':
                 descuento_tarjeta = 0.05
                 precio_fin = precio_fin * (1-descuento_tarjeta)
