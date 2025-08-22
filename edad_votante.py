8c5b022cebb03231c26647b2341ab59ea2caf7c0
ricardo-guanilo <ricardo.guanilo.p@uni.pe>
2025-08-22 15:58:12 -0500

    Actualización del archivo

--- edad_votante.py
+++ edad_votante.py
@@ -1,53 +1,57 @@
 
 try:
     edad = input('Ingrese su edad: ')
-    pais = input('''Ingrese el código según su país de residencia:
-    1. Perú
-    2. Colombia
-    3. Argentina
-    4. Corea del Sur
-    5. Japón
-    ''')
     edad = int(edad) #Verificar si la edad es entera para saltar error de ser necesario.
-    pais = int(pais) #Verificar si el código es entero para saltar error de ser necesario.
 except:
-    print(f'''ERROR: Ingrese un código de país adecuado.
-    Códigos permitidos: 1, 2, 3, 4, 5.
-    Código actual: {pais}''')
+    print('Ingrese una edad entera.')
 else:
-    match pais: # Casos según código de país correspondiente.
-        case 1:
-            nombre_pais = 'Perú'
-            if(int(edad)>=17):
-                print('Usted puede votar')
-            else:
-                print(f'Usted no puede votar, la edad mínima para votar en {nombre_pais} es 17, su edad es {edad}')
-        case 2:
-            nombre_pais = 'Colombia'
-            if(int(edad)>=18):
-                print('Usted puede votar')
-            else:
-                print(f'Usted no puede votar, la edad mínima para votar en {nombre_pais} es 18, su edad es {edad}')            
-        case 3:
-            nombre_pais = 'Argentina'
-            if(int(edad)>=16):
-                print('Usted puede votar')
-            else:
-                print(f'Usted no puede votar, la edad mínima para votar en {nombre_pais} es 16, su edad es {edad}')
-        case 4:
-            nombre_pais = 'Corea del Sur'
-            if(int(edad)>=19):
-                print('Usted puede votar')
-            else:
-                print(f'Usted no puede votar, la edad mínima para votar en {nombre_pais} es 19, su edad es {edad}')
-        case 5:
-            nombre_pais = 'Japón'
-            if(int(edad)>=18):
-                print('Usted puede votar')
-            else:
-                print(f'Usted no puede votar, la edad mínima para votar en {nombre_pais} es 18, su edad es {edad}')
-        case _: # Se ha ingresado un código no indicado.
-                print(f'''ERROR: Ingrese un código de país adecuado.
-    Códigos permitidos: 1, 2, 3, 4, 5.
-    Código actual: {pais}''') 
+    try:
+        pais = input('''Ingrese el código según su país de residencia:
+        1. Perú
+        2. Colombia
+        3. Argentina
+        4. Corea del Sur
+        5. Japón
+        ''')
+        pais = int(pais) #Verificar si el código es entero para saltar error de ser necesario.
+    except:
+        print(f'''ERROR: Ingrese un código de país adecuado.
+        Códigos permitidos: 1, 2, 3, 4, 5.
+        Código actual: {pais}''')
+    else:
+        match pais: # Casos según código de país correspondiente.
+            case 1:
+                nombre_pais = 'Perú'
+                if(int(edad)>=17):
+                    print('Usted puede votar')
+                else:
+                    print(f'Usted no puede votar, la edad mínima para votar en {nombre_pais} es 17, su edad es {edad}')
+            case 2:
+                nombre_pais = 'Colombia'
+                if(int(edad)>=18):
+                    print('Usted puede votar')
+                else:
+                    print(f'Usted no puede votar, la edad mínima para votar en {nombre_pais} es 18, su edad es {edad}')            
+            case 3:
+                nombre_pais = 'Argentina'
+                if(int(edad)>=16):
+                    print('Usted puede votar')
+                else:
+                    print(f'Usted no puede votar, la edad mínima para votar en {nombre_pais} es 16, su edad es {edad}')
+            case 4:
+                nombre_pais = 'Corea del Sur'
+                if(int(edad)>=19):
+                    print('Usted puede votar')
+                else:
+                    print(f'Usted no puede votar, la edad mínima para votar en {nombre_pais} es 19, su edad es {edad}')
+            case 5:
+                nombre_pais = 'Japón'
+                if(int(edad)>=18):
+                    print('Usted puede votar')
+                else:
+                    print(f'Usted no puede votar, la edad mínima para votar en {nombre_pais} es 18, su edad es {edad}')
+            case _: # Se ha ingresado un código no indicado.
+                    print(f'''ERROR: Ingrese un código de país adecuado.
+        Códigos permitidos: 1, 2, 3, 4, 5.
+        Código actual: {pais}''') 
                           
